57c0b0fb13d5f68f3db9ae3a355160e29667678c
Saihan Sadique <saihan.sadique1@gmail.com>
2024-04-12 16:14:05 +0530

    Update dependencies and add CORS middleware and Prometheus instrumentation

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 """ This module contains the FastAPI application instance and its configuration. """
 
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from starlette.middleware.cors import CORSMiddleware
 
 from api.handlers import movie_v1
 
@@ -11,7 +13,21 @@ def create_app():
     Returns:
         FastAPI: The configured FastAPI application object.
     """
+
     app = FastAPI(docs_url="/")
+
+    #middleware
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    Instrumentator().instrument(app).expose(app)
+
+
+
     # app.include_router(demo.router)
     app.include_router(movie_v1.router)
     return app
